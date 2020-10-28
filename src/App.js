68e1f7c5235d8c1a914ea205537c181ad12fc9c5
Johanna <svallingson@gmail.com>
2020-10-28 15:52:38 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component{
+    //The stateobject will contain properties for everything you want to store in the state
+    state = {
+        characters: [],
+    };
     render(){
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspiring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ];
-
+        const {characters} = this.state;
         return (
             <div className="container">
-                <Table characterData={characters}/>
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         )
     }
+    removeCharacter = (index) => {
+        const {characters} = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
+
 }
+
+
 
 export default App
--- src/Table.js
+++ src/Table.js
@@ -1,17 +1,4 @@
-import React, {Component} from 'react'
-
-class Table extends Component{
-    render(){
-        const {characterData} = this.props
-
-        return(
-           <table>
-               <TableHeader />
-               <TableBody characterData={characterData}/>
-           </table>
-        )
-    }
-}
+import React from 'react'
 
 const TableHeader = () => {
     return (
@@ -22,7 +9,7 @@ const TableHeader = () => {
         </tr>
         </thead>
     )
-}
+};
 
 const TableBody = (props) => {
     const rows = props.characterData.map((row,index) => {
@@ -30,10 +17,24 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
-    })
+    });
     return <tbody>{rows}</tbody>
-}
+};
+
+const Table = (props) => {
+    const {characterData, removeCharacter} = props;
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
+};
 
 export default Table
